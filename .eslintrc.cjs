386a8762bd0ebdabc7d91bf2319b2197bcfe5290
Artem Pivovarov <artem.pivovarov@alao.ch>
2023-11-10 09:10:56 +0700

    chore: add typescript checking

---
+++ .eslintrc.cjs
@@ -0,0 +1,45 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    '@nuxtjs/eslint-config-typescript',
+    'plugin:vue/vue3-recommended',
+    'eslint-config-prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['vue', 'prettier'],
+  rules: {
+    'prettier/prettier': 'warn',
+    'no-console': 'warn',
+    'lines-between-class-members': [
+      'error',
+      'always',
+      { exceptAfterSingleLine: true }
+    ],
+    'vue/multi-word-component-names': ['error'],
+    'vue/component-definition-name-casing': ['error', 'PascalCase'],
+    'vue/component-name-in-template-casing': [
+      'error',
+      'PascalCase',
+      { registeredComponentsOnly: false }
+    ],
+    'vue/component-tags-order': [
+      'error',
+      {
+        order: [
+          'script:not([setup])',
+          'script[setup]',
+          'template',
+          'style:not([scoped])',
+          'style[scoped]'
+        ]
+      }
+    ],
+  }
+}
---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  "$schema": "https://json.schemastore.org/prettierrc",
+  "semi": false,
+  "tabWidth": 2,
+  "singleQuote": true,
+  "trailingComma": "none",
+  "arrowParens": "avoid",
+  "singleAttributePerLine": true,
+  "endOfLine": "auto"
+}
